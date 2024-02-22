a41e325713ed75e22e175ca3d7d3e0717c869094
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2024-02-22 22:59:01 +0000

    Add defaults for budgey file path

--- src/config/local_config.rs
+++ src/config/local_config.rs
@@ -1,4 +1,14 @@
-struct LocalConfig {
+use std::env;
+
+#[derive(Debug)]
+pub struct LocalConfig {
     /// The path to the /budgey directory  
-    local_file_path: String,
+    pub budgey_dir: String,
+}
+
+impl Default for LocalConfig {
+    fn default() -> Self {
+        let budgey_dir = env!("HOME").to_string() + "/budgey";
+        LocalConfig { budgey_dir }
+    }
 }
