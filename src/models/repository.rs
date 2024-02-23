22ee34323d8d968593f210bdc0fd825b03689cb8
JordanH <jordanh@apadmi.com>
2024-02-23 12:33:16 +0000

    Add comment for where it will be stored

--- src/models/repository.rs
+++ src/models/repository.rs
@@ -1,6 +1,7 @@
 use super::pile::Pile;
 
 /// Represents a collection of piles that a user might have
+/// Stored in /budgey/[repo_name]/
 pub struct Repository {
     repo_name: String,
     pile_list: Vec<Pile>,
