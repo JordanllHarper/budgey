882d7bc92075e08a20fd41ed70c2114dbd08bc98
JordanH <jordanh@apadmi.com>
2024-02-24 17:41:49 +0000

    Create budget repository impl

--- src/repo/budget_repository.rs
+++ src/repo/budget_repository.rs
@@ -1,7 +1,29 @@
-use crate::models::budget::Budget;
+use crate::{config::local_config::LocalConfig, models::budget::Budget};
 
 trait BudgetRepository {
     fn create_new_budget(&self, budget: Budget);
     fn get_all_budgets(&self);
     fn delete_budget(&self, budget_name: &str);
+}
+
+struct BudgetRepositoryImpl {
+    local_config: LocalConfig,
+}
+
+impl BudgetRepositoryImpl {
+    fn new(local_config: LocalConfig) -> Self {
+        Self { local_config }
+    }
+}
+
+impl BudgetRepository for BudgetRepositoryImpl {
+    fn create_new_budget(&self, budget: Budget) {}
+
+    fn get_all_budgets(&self) {
+        todo!()
+    }
+
+    fn delete_budget(&self, budget_name: &str) {
+        todo!()
+    }
 }
