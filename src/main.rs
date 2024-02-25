d19169305b7d5177b045b2c33bc8a4d04b7565d2
JordanH <jordanh@apadmi.com>
2024-02-25 22:12:34 +0000

    Remove result from ain

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ pub mod models;
 pub mod repo;
 pub mod utils;
 
-fn main() -> Result<(), ()> {
+fn main() {
     let init_handler: InitHandlerImpl = InitHandlerImpl::new(LocalConfig::test());
     let commands = budgeter_cli::BudgeyCLI::parse().commands;
     let result = match commands {
@@ -22,9 +22,7 @@ fn main() -> Result<(), ()> {
     };
     if let Err(e) = result {
         print_error(&e);
-        return Err(());
     };
-    Ok(())
 }
 fn print_error(error: &impl HandlerError) {
     println!("{}", error.get_user_message());
