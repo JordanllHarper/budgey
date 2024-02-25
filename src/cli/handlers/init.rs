52f060aa5b86969417fe5e53157db3cfec011652
JordanH <jordanh@apadmi.com>
2024-02-25 22:12:29 +0000

    Create file and budget directory with error handling

--- src/cli/handlers/init.rs
+++ src/cli/handlers/init.rs
@@ -1,24 +1,30 @@
+use std::io;
+
 use crate::{
     config::local_config::LocalConfig,
     utils::{
         error::HandlerError,
-        io::{create_budgey_dir_if_not_exists, create_named_budget_dir_if_not_exists},
+        io::{create_budgey_dir_if_not_exists, create_named_budget_dir},
     },
 };
 
 pub enum InitHandlerError {
     CreateBudgeyDirFailed,
     CreateNamedBudgetDirFailed,
+    BudgetDirectoryAlreadyExists,
 }
 impl HandlerError for InitHandlerError {
     fn get_user_message(&self) -> String {
         match self {
             InitHandlerError::CreateBudgeyDirFailed => {
-                String::from("Couldn't create the budgey directory. Exiting...")
+                String::from("Something went wrong when trying to initialise the budgey directory.")
             }
 
-            InitHandlerError::CreateNamedBudgetDirFailed => {
-                String::from("Couldn't create the new budget's directory. Exiting...")
+            InitHandlerError::CreateNamedBudgetDirFailed => String::from(
+                "Something went wrong when trying to initialise the new budget directory.",
+            ),
+            InitHandlerError::BudgetDirectoryAlreadyExists => {
+                String::from("That budget name was already found in your budgey directory.")
             }
         }
     }
@@ -42,14 +48,19 @@ impl InitHandlerImpl {
 // goes wrong
 impl InitHandler for InitHandlerImpl {
     fn handle(&self, budget_name: &str) -> anyhow::Result<(), InitHandlerError> {
-        if let Err(_) = create_budgey_dir_if_not_exists(&self.local_config.budgey_dir) {
-            return Err(InitHandlerError::CreateBudgeyDirFailed);
-        }
+        create_budgey_dir_if_not_exists(&self.local_config.budgey_dir)
+            .map_err(|_| InitHandlerError::CreateBudgeyDirFailed)?;
 
-        match create_named_budget_dir_if_not_exists(&self.local_config.budgey_dir, budget_name) {
-            Err(_) => Err(InitHandlerError::CreateNamedBudgetDirFailed),
-            Ok(_) => Ok(()),
-        }
+        create_named_budget_dir(&self.local_config.budgey_dir, budget_name).map_err(|e| {
+            if e.kind() == io::ErrorKind::AlreadyExists {
+                InitHandlerError::BudgetDirectoryAlreadyExists
+            } else {
+                InitHandlerError::CreateNamedBudgetDirFailed
+            }
+        })?;
+
         // TODO: Create a main pile
+        //
+        Ok(())
     }
 }
