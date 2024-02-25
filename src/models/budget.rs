73fc40b39b11613ac5f66d58a5459dc83071c9dc
JordanH <jordanh@apadmi.com>
2024-02-25 22:48:08 +0000

    Renaming and using &str instead of String

--- src/models/budget.rs
+++ src/models/budget.rs
@@ -1,16 +1,19 @@
+use serde::{Deserialize, Serialize};
+
 use super::pile::Pile;
 
 /// Represents a collection of piles that a user might have
-/// Stored in /budgey/[repo_name]/
+/// Stored in /budgey/[budget_name]/
+#[derive(Serialize, Deserialize)]
 pub struct Budget {
     repo_name: String,
     pile_list: Vec<Pile>,
 }
 
 impl Budget {
-    pub fn new(repo_name: String, pile_list: Vec<Pile>) -> Self {
+    pub fn new(budget_name: &str, pile_list: Vec<Pile>) -> Self {
         Self {
-            repo_name,
+            repo_name: budget_name.to_string(),
             pile_list,
         }
     }
