6df9be2b462e56d32581408970886c4659aa56f8
JordanH <jordanh@apadmi.com>
2024-02-27 14:06:49 +0000

    Readd serialisation back

--- src/models/budget.rs
+++ src/models/budget.rs
@@ -1,20 +1,33 @@
+use nanoid::nanoid;
 use serde::{Deserialize, Serialize};
 
 use super::pile::Pile;
+
+#[derive(Serialize, Deserialize)]
+pub struct BudgetDetail {
+    id: String,
+    pub budget_name: String,
+}
+
+impl BudgetDetail {
+    fn new(id: String, budget_name: String) -> Self {
+        Self { id, budget_name }
+    }
+}
 
 /// Represents a collection of piles that a user might have
 /// Stored in /budgey/[budget_name]/
 #[derive(Serialize, Deserialize)]
 pub struct Budget {
-    repo_name: String,
-    pile_list: Vec<Pile>,
+    pub budget_detail: BudgetDetail,
+    pub pile_list: Vec<Pile>,
 }
 
 impl Budget {
-    pub fn new(budget_name: &str, pile_list: Vec<Pile>) -> Self {
+    pub fn new(budget_name: &str) -> Self {
         Self {
-            repo_name: budget_name.to_string(),
-            pile_list,
+            budget_detail: BudgetDetail::new(nanoid!(), budget_name.to_string()),
+            pile_list: Pile::new_with_main(),
         }
     }
 }
