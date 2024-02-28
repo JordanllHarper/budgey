d82cf67cdb91271328dd73da293ae57ae3326be6
JordanH <jordanh@apadmi.com>
2024-02-28 09:41:55 +0000

    Add pile repository to the init use case

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,26 @@ use cli::{
     budgeter_cli,
     use_cases::init::{InitUseCase, InitUseCaseImpl},
 };
-use repo::budget_repository::BudgetRepositoryImpl;
+use repo::{budget_repository::BudgetRepositoryImpl, pile_repository::PileRepositoryImpl};
 use utils::error::UseCaseError;
 
 use crate::config::local_config::LocalConfig;
 
-pub mod budget_io;
 pub mod cli;
 pub mod config;
+pub mod io_operations;
 pub mod models;
 pub mod repo;
 pub mod utils;
 
 fn main() {
     let local_config = LocalConfig::test();
-    let init_use_case: InitUseCaseImpl<BudgetRepositoryImpl> =
-        InitUseCaseImpl::new(BudgetRepositoryImpl::new(&local_config.budgey_dir));
+    let budgey_path = &local_config.budgey_dir;
+    let init_use_case: InitUseCaseImpl<BudgetRepositoryImpl, PileRepositoryImpl> =
+        InitUseCaseImpl::new(
+            BudgetRepositoryImpl::new(budgey_path),
+            PileRepositoryImpl::new(budgey_path),
+        );
     let commands = budgeter_cli::BudgeyCLI::parse().commands;
     let result = match commands {
         budgeter_cli::Commands::Init { name } => init_use_case.handle(&name),
