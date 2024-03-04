a8e1da3fdbb42ba828301405c6351197d0cde9c5
JordanH <jordanh@apadmi.com>
2024-03-04 23:00:22 +0000

    Changes to path to address bugs

--- src/repo/budget_repository.rs
+++ src/repo/budget_repository.rs
@@ -1,6 +1,6 @@
 use std::{fs, io::ErrorKind};
 
-use crate::models::budget::Budget;
+use crate::models::budget::{self, Budget};
 
 pub enum CreateNewBudgetError {
     CreateNamedBudgetDirFailed,
@@ -33,8 +33,9 @@ impl BudgetRepositoryImpl {
 
 impl BudgetRepository for BudgetRepositoryImpl {
     fn create_new_budget(&self, budget: Budget) -> Result<(), CreateNewBudgetError> {
+        let budgey_directory = &self.budgey_directory;
         let budget_name = &budget.budget_detail.budget_name;
-        let budget_path = format!("{}/{}", self.budgey_directory, budget_name);
+        let budget_path = format!("{}/{}", budgey_directory, budget_name);
         fs::create_dir(&budget_path).map_err(|e| {
             if let ErrorKind::AlreadyExists = e.kind() {
                 CreateNewBudgetError::BudgetDirectoryAlreadyExists
@@ -45,8 +46,7 @@ impl BudgetRepository for BudgetRepositoryImpl {
         let json_file = format!("{}/{}.json", &budget_path, budget_name);
         fs::write(json_file, serde_json::to_string(&budget).unwrap())
             .map_err(|_| CreateNewBudgetError::CouldntWriteJson)?;
-
-        todo!()
+        Ok(())
     }
 
     fn get_all_budgets(&self) -> Result<Vec<std::string::String>, BudgetRepositoryError> {
