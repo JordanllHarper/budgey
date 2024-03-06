fc9ddcab8c4b58ec964d430649bc05aefeef4a25
JordanH <jordanh@apadmi.com>
2024-03-06 21:36:35 +0000

    Start building out pile use case impl

--- src/cli/use_cases/pile.rs
+++ src/cli/use_cases/pile.rs
@@ -1,11 +1,30 @@
-use crate::cli::budgeter_cli::{PileOperationSubcommand, PileSubcommand};
+use crate::{
+    cli::budgeter_cli::{PileOperationSubcommand, PileSubcommand},
+    repo::{
+        budget_repository::BudgetRepository, budgey_repository::BudgeyRepository,
+        pile_repository::PileRepository,
+    },
+};
 
 pub trait PileUseCase {
-    fn handle(&self, subcommand: PileSubcommand) -> anyhow::Result<()>;
+    fn handle(&self, budget: &str, subcommand: PileSubcommand) -> anyhow::Result<()>;
+}
+pub struct PileUseCaseImpl<'a> {
+    pile_repo: &'a dyn PileRepository,
+    budget_repo: &'a dyn BudgetRepository,
+}
+
+impl<'a> PileUseCaseImpl<'a> {
+    pub fn new(pile_repo: &'a dyn PileRepository, budget_repo: &'a dyn BudgetRepository) -> Self {
+        Self {
+            pile_repo,
+            budget_repo,
+        }
+    }
 }
 // TODO: create handlers and delegate
-impl dyn PileUseCase {
-    fn handle(&self, subcommand: PileSubcommand) -> anyhow::Result<()> {
+impl<'a> PileUseCase for PileUseCaseImpl<'a> {
+    fn handle(&self, budget: &str, subcommand: PileSubcommand) -> anyhow::Result<()> {
         match subcommand {
             PileSubcommand::New {
                 name,
@@ -13,27 +32,27 @@ impl dyn PileUseCase {
                 budget,
             } => todo!(),
             PileSubcommand::PileOperation { budget, subcommand } => {
-                Self::handle_pile_op_subcommand(subcommand)
+                handle_pile_op_subcommand(subcommand)
             }
         }
     }
+}
 
-    fn handle_pile_op_subcommand(subcommand: PileOperationSubcommand) -> anyhow::Result<()> {
-        match subcommand {
-            PileOperationSubcommand::Push { amount, source } => todo!(),
-            PileOperationSubcommand::Pull { amount, usage } => todo!(),
-            PileOperationSubcommand::Merge {
-                amount,
-                source,
-                destination,
-                delete_after_merge,
-                usage,
-            } => todo!(),
-            PileOperationSubcommand::Balance { name } => todo!(),
-            PileOperationSubcommand::List => todo!(),
-            PileOperationSubcommand::View { name } => todo!(),
-            PileOperationSubcommand::Remove { name } => todo!(),
-        }
-        todo!()
+pub fn handle_pile_op_subcommand(subcommand: PileOperationSubcommand) -> anyhow::Result<()> {
+    match subcommand {
+        PileOperationSubcommand::Push { amount, source } => todo!(),
+        PileOperationSubcommand::Pull { amount, usage } => todo!(),
+        PileOperationSubcommand::Merge {
+            amount,
+            source,
+            destination,
+            delete_after_merge,
+            usage,
+        } => todo!(),
+        PileOperationSubcommand::Balance { name } => todo!(),
+        PileOperationSubcommand::List => todo!(),
+        PileOperationSubcommand::View { name } => todo!(),
+        PileOperationSubcommand::Remove { name } => todo!(),
     }
+    todo!()
 }
