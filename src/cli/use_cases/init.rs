9c4949ff8be00b32991b2c437997aa6829753d9a
JordanH <jordanh@apadmi.com>
2024-03-06 21:02:52 +0000

    Convert to using thiserror and display for message

--- src/cli/use_cases/init.rs
+++ src/cli/use_cases/init.rs
@@ -1,34 +1,22 @@
+use crate::repo::pile_repository::{CreateNewPileError, PileRepository};
 use crate::{
     models::{budget::Budget, pile::Pile},
     repo::{
         budget_repository::{BudgetRepository, CreateNewBudgetError},
         budgey_repository::{BudgeyRepository, InitBudgeyError},
-        pile_repository::{CreateNewPileError, PileRepository},
     },
-    utils::error::GenericError,
 };
 
+#[derive(Debug, thiserror::Error)]
 pub enum InitError {
     CreateBudgeyError(InitBudgeyError),
     CreateBudgetError(CreateNewBudgetError),
     CreatePileError(CreateNewPileError),
 }
-impl InitError {
-    pub fn new_from_budgey_error(e: InitBudgeyError) -> InitError {
-        InitError::CreateBudgeyError(e)
-    }
-    pub fn new_from_budget_error(e: CreateNewBudgetError) -> InitError {
-        InitError::CreateBudgetError(e)
-    }
 
-    pub fn new_from_pile_error(e: CreateNewPileError) -> InitError {
-        InitError::CreatePileError(e)
-    }
-}
-
-impl GenericError for InitError {
-    fn get_user_message(&self) -> String {
-        match self {
+impl std::fmt::Display for InitError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let message = match self {
             InitError::CreateBudgetError(init_error) => match init_error {
                 CreateNewBudgetError::CreateNamedBudgetDirFailed => String::from(
                     "Something went wrong when trying to initialise the new budget directory.",
@@ -62,12 +50,27 @@ impl GenericError for InitError {
                     String::from("The budgey directory couldn't be created.")
                 }
             },
-        }
+        };
+        f.write_str(&message)?;
+        Ok(())
+    }
+}
+
+impl InitError {
+    pub fn new_from_budgey_error(e: InitBudgeyError) -> InitError {
+        InitError::CreateBudgeyError(e)
+    }
+    pub fn new_from_budget_error(e: CreateNewBudgetError) -> InitError {
+        InitError::CreateBudgetError(e)
+    }
+
+    pub fn new_from_pile_error(e: CreateNewPileError) -> InitError {
+        InitError::CreatePileError(e)
     }
 }
 
 pub trait InitUseCase {
-    fn handle(&self, repo_name: &str) -> anyhow::Result<(), InitError>;
+    fn handle(&self, repo_name: &str) -> anyhow::Result<()>;
 }
 pub struct InitUseCaseImpl<'a> {
     budget_repo: &'a dyn BudgetRepository,
@@ -93,7 +96,7 @@ impl<'a> InitUseCaseImpl<'a> {
 // (not what specifically the issue was), this would be good for helping new users if something
 // goes wrong
 impl<'a> InitUseCase for InitUseCaseImpl<'a> {
-    fn handle(&self, budget_name: &str) -> Result<(), InitError> {
+    fn handle(&self, budget_name: &str) -> anyhow::Result<()> {
         let result = self.budgey_repo.init_budgey();
         if let Err(e) = result {
             match e {
@@ -101,7 +104,7 @@ impl<'a> InitUseCase for InitUseCaseImpl<'a> {
                     println!("Found budgey directory already. Creating new budget under name...")
                 }
                 InitBudgeyError::BudgeyCreationFailed => {
-                    return Err(InitError::new_from_budgey_error(e));
+                    return Err(InitError::new_from_budgey_error(e).into());
                 }
             }
         }
