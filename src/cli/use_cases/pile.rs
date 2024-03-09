8c39a4bdaf4a6212b9f41ced0454b56df125c46c
JordanH <jordanh@apadmi.com>
2024-03-09 20:22:46 +0000

    Add errors and use

--- src/cli/use_cases/pile.rs
+++ src/cli/use_cases/pile.rs
@@ -1,19 +1,66 @@
-use crate::cli::budgeter_cli::{PileOperationSubcommand, PileSubcommand};
+use std::fmt::{Display, Formatter};
 
-fn handle_pile_command(budget: &str, subcommand: PileSubcommand) -> anyhow::Result<()> {
+use crate::{
+    cli::budgeter_cli::{PileOperationSubcommand, PileSubcommand},
+    handling::pile_handling::{create_new_pile, CreateNewPileError},
+    models::pile::{Pile, PileType},
+};
+
+#[derive(Debug)]
+pub enum PileError {
+    CreateNewPileError(CreateNewPileError),
+    PileOperationError(PileOperationError),
+}
+impl PileError {
+    pub fn new_from_create_new_pile_error(e: CreateNewPileError) -> Self {
+        PileError::CreateNewPileError(e)
+    }
+}
+
+impl Display for PileError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+pub fn handle_pile_command(
+    budgey_path: &str,
+    subcommand: PileSubcommand,
+) -> anyhow::Result<(), PileError> {
     match subcommand {
         PileSubcommand::New {
             name,
             initial_balance,
             budget,
-        } => todo!(),
+        } => {
+            let pile = Pile::new(
+                name,
+                initial_balance.unwrap_or(0.0),
+                PileType::UserCreated,
+                vec![],
+            );
+
+            let result = create_new_pile(pile, &budget, budgey_path);
+            todo!()
+        }
         PileSubcommand::PileOperation { budget, subcommand } => {
-            handle_pile_op_subcommand(subcommand)
+            handle_pile_op_subcommand(subcommand);
+            todo!()
         }
     }
 }
 
-pub fn handle_pile_op_subcommand(subcommand: PileOperationSubcommand) -> anyhow::Result<()> {
+#[derive(Debug)]
+pub enum PileOperationError {
+    PushError,
+    PullError,
+    MergeError,
+    BalanceError,
+    ListError,
+    ViewError,
+    RemoveError,
+}
+fn handle_pile_op_subcommand(subcommand: PileOperationSubcommand) -> anyhow::Result<()> {
     match subcommand {
         PileOperationSubcommand::Push { amount, source } => todo!(),
         PileOperationSubcommand::Pull { amount, usage } => todo!(),
