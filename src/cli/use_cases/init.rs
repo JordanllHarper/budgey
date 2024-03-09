efc4872e0121d1850347a051e30893b53b987505
JordanH <jordanh@apadmi.com>
2024-03-09 19:28:20 +0000

    Refactor to add closures and test

--- src/cli/use_cases/init.rs
+++ src/cli/use_cases/init.rs
@@ -1,8 +1,7 @@
 use crate::{
     handling::{
-        budget_handling::{create_new_budget, CreateNewBudgetError},
-        budgey_handling::{init_budgey, InitBudgeyError},
-        pile_handling::{create_new_pile, CreateNewPileError},
+        budget_handling::CreateNewBudgetError, budgey_handling::InitBudgeyError,
+        pile_handling::CreateNewPileError,
     },
     models::{budget::Budget, pile::Pile},
 };
@@ -69,12 +68,26 @@ impl InitError {
     }
 }
 
-pub fn handle_init(budgey_path: &str, budget_name: &str) -> anyhow::Result<()> {
+pub fn handle_init(
+    budgey_path: &str,
+    budget_name: &str,
+    init_budgey: fn(budgey_path: &str) -> Result<(), InitBudgeyError>,
+    create_new_budget: fn(
+        budgey_directory: &str,
+        budget: Budget,
+    ) -> Result<(), CreateNewBudgetError>,
+    create_new_pile: fn(
+        pile: Pile,
+        budget_name: &str,
+        budgey_path: &str,
+    ) -> Result<(), CreateNewPileError>,
+) -> anyhow::Result<(), InitError> {
     let result = init_budgey(budgey_path);
     if let Err(e) = result {
         match e {
             InitBudgeyError::BudgeyAlreadyExists => {
-                println!("Found budgey directory already. Creating new budget under name...")
+                // TODO: This should be returned to allow for testing
+                println!("Found budgey directory already. Creating new budget under name...");
             }
             InitBudgeyError::BudgeyCreationFailed => {
                 return Err(InitError::new_from_budgey_error(e).into());
@@ -88,4 +101,80 @@ pub fn handle_init(budgey_path: &str, budget_name: &str) -> anyhow::Result<()> {
         .map_err(|e| InitError::new_from_pile_error(e))?;
 
     Ok(())
+}
+#[cfg(test)]
+pub mod init_tests {
+    use crate::handling::{
+        budget_handling::CreateNewBudgetError, budgey_handling::InitBudgeyError,
+        pile_handling::CreateNewPileError,
+    };
+
+    #[test]
+    pub fn handle_init_success() {
+        let budgey_path = "test_budgey";
+        let budget_name = "test_budget";
+        let result = super::handle_init(
+            budgey_path,
+            budget_name,
+            |_| Ok(()),
+            |_, _| Ok(()),
+            |_, _, _| Ok(()),
+        );
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    pub fn handle_init_success_with_budgey_already_exists() {
+        let budgey_path = "test_budgey";
+        let budget_name = "test_budget";
+        let result = super::handle_init(
+            budgey_path,
+            budget_name,
+            |_| Err(InitBudgeyError::BudgeyAlreadyExists),
+            |_, _| Ok(()),
+            |_, _, _| Ok(()),
+        );
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    pub fn handle_init_failure_budgey_creation_fails() {
+        let budgey_path = "test_budgey";
+        let budget_name = "test_budget";
+        let result = super::handle_init(
+            budgey_path,
+            budget_name,
+            |_| Err(InitBudgeyError::BudgeyCreationFailed),
+            |_, _| Ok(()),
+            |_, _, _| Ok(()),
+        );
+        assert!(result.is_err());
+    }
+
+    #[test]
+    pub fn handle_init_failure_create_budget_fails() {
+        let budgey_path = "test_budgey";
+        let budget_name = "test_budget";
+        let result = super::handle_init(
+            budgey_path,
+            budget_name,
+            |_| Ok(()),
+            |_, _| Err(CreateNewBudgetError::CreateNamedBudgetDirFailed),
+            |_, _, _| Ok(()),
+        );
+        assert!(result.is_err());
+    }
+    #[test]
+    pub fn handle_init_failure_pile_creation_fails() {
+        let budgey_path = "test_budgey";
+        let budget_name = "test_budget";
+        let result = super::handle_init(
+            budgey_path,
+            budget_name,
+            |_| Ok(()),
+            |_, _| Ok(()),
+            |_, _, _| Err(CreateNewPileError::CouldntCreatePileDirectory),
+        );
+        assert!(result.is_err());
+    }
 }
