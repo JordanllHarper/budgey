7f859c74eac88e862710422c59e4c105cece9e1c
JordanH <jordanh@apadmi.com>
2024-03-10 11:06:13 +0000

    Update pile new method for removed fields

--- src/models/pile.rs
+++ src/models/pile.rs
@@ -1,18 +1,15 @@
 use serde::{Deserialize, Serialize};
 
-use super::transaction::Transaction;
-
 #[derive(Serialize, Deserialize)]
 pub struct Pile {
     pub name: String,
     pub balance: f32,
     pub pile_type: PileType,
-    pub pile_transaction_history: Vec<Transaction>,
 }
 
 impl Default for Pile {
     fn default() -> Self {
-        Pile::new("main".to_string(), 0.0, PileType::Main, vec![])
+        Pile::new("main".to_string(), 0.0, PileType::Main)
     }
 }
 
@@ -23,20 +20,14 @@ pub enum PileType {
 }
 
 impl Pile {
-    pub fn new(
-        name: String,
-        balance: f32,
-        pile_type: PileType,
-        transaction_history: Vec<Transaction>,
-    ) -> Self {
+    pub fn new(name: String, balance: f32, pile_type: PileType) -> Self {
         Self {
             name,
             balance,
             pile_type,
-            pile_transaction_history: transaction_history,
         }
     }
     pub fn default_main_pile() -> Pile {
-        Pile::new("main".to_string(), 0.0, PileType::Main, vec![])
+        Pile::new("main".to_string(), 0.0, PileType::Main)
     }
 }
