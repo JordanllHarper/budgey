e347394d6277d25b84e40a5987676e747d2be3b2
JordanH <jordanh@apadmi.com>
2024-03-11 09:31:34 +0000

    Add pile add handling and remove pile new pull and push files

---
+++ src/cli/use_cases/pile_sub/pile_add.rs
@@ -0,0 +1,7 @@
+use crate::models::pile::Pile;
+
+pub fn pile_add_transaction(pile: Pile, amount: f64, budget: &str, budgey_path: &str) -> Pile {
+    let mut records = pile.records;
+
+    todo!()
+}
