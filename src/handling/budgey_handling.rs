9c26100c6ea605ac474f988d2a4c31388f10e626
JordanH <jordanh@apadmi.com>
2024-03-14 16:12:00 +0000

    Move config handling into budgey handling

--- src/handling/budgey_handling.rs
+++ src/handling/budgey_handling.rs
@@ -1,5 +1,7 @@
 use std::{fs, io::ErrorKind};
 
+use crate::{models::budget_collection::BudgetCollection, utils::json_utils::create_json_path};
+
 #[derive(Debug)]
 pub enum InitBudgeyError {
     BudgeyAlreadyExists,
@@ -16,5 +18,25 @@ pub fn init_budgey(budgey_path: &str) -> anyhow::Result<(), InitBudgeyError> {
             InitBudgeyError::BudgeyCreationFailed
         }
     })?;
+    let budget_collection_json_path = create_json_path(budgey_path, "budget_collection");
+    let budgets_collection = BudgetCollection::new_init();
     Ok(())
+}
+
+use std::env;
+
+#[derive(Debug)]
+pub struct LocalConfig {
+    /// The path to the /budgey directory  
+    pub budgey_dir: String,
+}
+
+impl Default for LocalConfig {
+    fn default() -> Self {
+        let budgey_dir = env!("HOME").to_string() + "/budgey";
+        LocalConfig { budgey_dir }
+    }
+}
+pub fn get_local_config() -> LocalConfig {
+    todo!()
 }
