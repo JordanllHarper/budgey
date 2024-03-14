d92ae4a80c7e84bb2f18c43abe9174ac919cd241
JordanH <jordanh@apadmi.com>
2024-03-14 15:42:00 +0000

    Add BudgetCollection and impl

--- src/models/budget_collection.rs
+++ src/models/budget_collection.rs
@@ -2,3 +2,24 @@
 pub struct BudgetCollection {
     pub budget_names: Vec<String>,
 }
+
+impl BudgetCollection {
+    pub fn new(budget_names: Vec<String>) -> Self {
+        Self { budget_names }
+    }
+
+    pub fn add_name(self, name: String) -> Self {
+        let budget_names = self
+            .budget_names
+            .into_iter()
+            .chain(std::iter::once(name))
+            .collect::<_>();
+        BudgetCollection::new(budget_names)
+    }
+
+    pub fn new_init() -> Self {
+        Self {
+            budget_names: vec!["main".to_string()],
+        }
+    }
+}
