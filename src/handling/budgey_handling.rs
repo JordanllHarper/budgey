131e03fc4cda2a4b2a314dc2f84f6ac18a938698
JordanH <jordanh@apadmi.com>
2024-03-15 13:05:57 +0000

    Rename to budgey state name

--- src/handling/budgey_handling.rs
+++ src/handling/budgey_handling.rs
@@ -18,7 +18,7 @@ pub enum CheckBudgeyExistsError {
 }
 pub fn init_budgey(
     budgey_path: &str,
-    budget_state_name: &str,
+    budgey_state_name: &str,
 ) -> anyhow::Result<(), InitBudgeyError> {
     fs::create_dir(&budgey_path).map_err(|e| {
         if let ErrorKind::AlreadyExists = e.kind() {
