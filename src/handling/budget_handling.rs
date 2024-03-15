c6d861d1d3c2eb7440db667c54c2fbb89d291dc5
JordanH <jordanh@apadmi.com>
2024-03-15 11:04:21 +0000

    Move get budget back to budget handling

--- src/handling/budget_handling.rs
+++ src/handling/budget_handling.rs
@@ -9,6 +9,22 @@ pub enum BudgetError {
 }
 
 #[derive(Debug)]
+pub enum GetBudgetError {
+    GetBudgetFileError,
+    DeserialieBudgetJsonError,
+}
+pub fn get_budget(budgey_path: &str, budget_name: &str) -> anyhow::Result<Budget, BudgetError> {
+    let budget_file_path = format!("{}/{}", budgey_path, budget_name);
+    let budget_path = create_json_path(&budget_file_path, budget_name);
+    let budget_json = fs::read_to_string(budget_path).map_err(|_| {
+        budget_handling::BudgetError::GetBudgetError(GetBudgetError::GetBudgetFileError)
+    })?;
+    let budget: Budget = serde_json::from_str(&budget_json)
+        .map_err(|_| BudgetError::GetBudgetError(GetBudgetError::DeserialieBudgetJsonError))?;
+    Ok(budget)
+}
+
+#[derive(Debug)]
 pub enum CreateNewBudgetError {
     CreateNamedBudgetDirFailed,
     BudgetDirectoryAlreadyExists,
