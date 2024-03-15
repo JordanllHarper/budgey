9ac42924ca10a8c00eab277b547e218d727e3d2c
JordanH <jordanh@apadmi.com>
2024-03-15 10:54:52 +0000

    Add get_budgey_state

--- src/handling/budget_handling.rs
+++ src/handling/budget_handling.rs
@@ -15,6 +15,28 @@ pub enum BudgetError {
     CreateNewBudgetError(CreateNewBudgetError),
 }
 
+pub enum GetBudgeyStateError {
+    BudgeyStateFileNotFound,
+    ErrorReadingBudgeyState,
+    DeserialiseBudgeyStateError,
+}
+pub fn get_budgey_state(
+    budgey_directory: &str,
+) -> anyhow::Result<budget_collection::BudgeyState, GetBudgeyStateError> {
+    let budget_state_name: String = String::from("budgey_state");
+    let budgey_state_path = create_json_path(budgey_directory, &budget_state_name);
+    let result = fs::read_to_string(budgey_state_path).map_err(|e| {
+        if e.kind() == ErrorKind::NotFound {
+            GetBudgeyStateError::BudgeyStateFileNotFound
+        } else {
+            GetBudgeyStateError::ErrorReadingBudgeyState
+        }
+    })?;
+    let budgey_state: budget_collection::BudgeyState = serde_json::from_str(&result)
+        .map_err(|_| GetBudgeyStateError::DeserialiseBudgeyStateError)?;
+    Ok(budgey_state)
+}
+
 #[derive(Debug)]
 pub enum GetBudgetError {
     GetBudgetFileError,
@@ -49,7 +71,20 @@ pub fn create_new_budget(
         .map_err(|_| CreateNewBudgetError::CouldntWriteJson)?;
     Ok(())
 }
-pub fn get_all_budgets() -> anyhow::Result<Vec<String>, BudgetRepositoryError> {
+#[derive(Debug)]
+pub enum GetAllBudgetsError {
+    BudgeyDirNotFound,
+    ReadDirError,
+    UnknownError,
+}
+
+pub fn get_all_budgets(budgey_directory: &str) -> anyhow::Result<Vec<Budget>, BudgetError> {
+    let budgey_dir = fs::read_dir(budgey_directory).map_err(|e| {
+        if e.kind() == ErrorKind::NotFound {
+            return BudgetError::GetAllBudgetsError(GetAllBudgetsError::BudgeyDirNotFound);
+        }
+        BudgetError::GetAllBudgetsError(GetAllBudgetsError::UnknownError)
+    })?;
     todo!()
 }
 pub fn delete_budget(budget_name: &str) -> anyhow::Result<(), BudgetRepositoryError> {
