c90dd7568b179469f1d2a78efa9b9beed6a5f5b9
JordanH <jordanh@apadmi.com>
2024-03-15 11:38:03 +0000

    Remove pile sub

--- src/cli/use_cases/pile_sub/pile_add.rs
+++
@@ -1,7 +0,0 @@
-use crate::models::pile::Pile;
-
-pub fn pile_add_transaction(pile: Pile, amount: f64, budget: &str, budgey_path: &str) -> Pile {
-    let mut records = pile.records;
-
-    todo!()
-}
